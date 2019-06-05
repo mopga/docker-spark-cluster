15b3a70fdc318e866d29e2708eebd5d693d57a76
Yakov Budymko <mopgobopot@gmail.com>
2019-06-05 06:59:40 +0000

    changed to spark 2.3.0, add start script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#add hostname and iterface ip to hosts
+echo -e "$(ip -o route get 8.8.8.8 | sed -e 's/^.* src \([^ ]*\) .*$/\1/') $(hostname)" >> /etc/hosts
+
+#start all cluster
+docker-compose up -d
+
